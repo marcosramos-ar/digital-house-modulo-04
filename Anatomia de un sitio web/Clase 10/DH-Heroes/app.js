9dcc2e62e2ec7950abbd4e953f764672367f1f20
Marcos Ramos <marcosramos.dm@gmail.com>
2022-03-10 22:47:37 -0300

    App

--- Anatomia de un sitio web/Clase 10/DH-Heroes/app.js
+++ Anatomia de un sitio web/Clase 10/DH-Heroes/app.js
@@ -9,3 +9,11 @@ app.listen(3030, () =>
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "./views/index.html"));
 });
+
+app.get("/lovelace", (req,res) => {res.sendFile(path.join(__dirname, "./views/lovelace.html"));});
+app.get("/turing", (req,res) => {res.sendFile(path.join(__dirname, "./views/turing.html"));});
+app.get("/berners-lee", (req,res) => {res.sendFile(path.join(__dirname, "./views/berners-lee.html"));});
+app.get("/babbage", (req,res) => {res.sendFile(path.join(__dirname, "./views/babbage.html"));});
+app.get("/clarke", (req,res) => {res.sendFile(path.join(__dirname, "./views/clarke.html"));});
+app.get("/hamilton", (req,res) => {res.sendFile(path.join(__dirname, "./views/hamilton.html"));});
+app.get("/hopper", (req,res) => {res.sendFile(path.join(__dirname, "./views/hopper.html"));});
