e14d63f1759345a7dc3294d99e5e19bea7eea734
Marcos Ramos <marcosramos.dm@gmail.com>
2022-03-15 23:24:39 -0300

    Ejercicios Sitio Mercado Liebre

---
+++ Anatomia de un sitio web/Clase 11/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, () => 
+    console.log("Operación exitosa.")
+);
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "./views/home.html"));
+});
